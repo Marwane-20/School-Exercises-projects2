811dd382230ce661f7f73afdcc2247119bfea99d
Marwane-20 <83011737+Marwane-20@users.noreply.github.com>
2021-10-23 19:47:00 +0100

    Concept classes

---
+++ Class c#.cs
@@ -0,0 +1,51 @@
+using System;
+namespace exrcice
+{
+    public class Program
+    {
+      public static void Main()
+      {
+      }
+    }
+
+    public class Etudiant
+      {
+          private string CNE;
+
+      }
+     public class Option_filiere
+      {
+          public void PlaceDispo()
+          {
+              throw new System.Exception("Not implemented");
+          }
+
+          public void IsFull()
+          {
+              throw new System.Exception("Not implemented");
+          }
+
+          private int capacité;
+
+      }
+     public class Score_merite
+      {
+          private int notePA;
+
+          private int ordre;
+
+      }
+     public class Option1 : Option_filiere
+      {
+      }
+     public class Option2 : Option_filiere
+      {
+      }
+     public class Option3 : Option_filiere
+      {
+      }
+    
+  
+  
+}
+
